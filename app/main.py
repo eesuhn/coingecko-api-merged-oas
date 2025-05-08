6608b53b4b7652f388a30da7392513841ba30156
eesuhn <eason.yihong@gmail.com>
2025-05-09 00:03:23 +0800

    Select list of desired paths

--- app/main.py
+++ app/main.py
@@ -4,8 +4,24 @@ from .oas_merger import OASMerger
 from .endpoint_selector import EndpointSelector
 
 DESIRED_PATHS = [
-    "/ping",
-    "/coins/list"
+    # General
+    "/key",  # 💼 API Usage
+    "/simple/price",  # Coin Price by IDs and Symbols
+    "/simple/token_price/{id}",  # Coin Price by Token Addresses
+    "/coins/list",  # Coins List (ID Map)
+    "/coins/{id}",  # Coin Data by ID
+    "/coins/{id}/market_chart/range",  # Coin Historical Chart Data within Time Range by ID
+    "/coins/{id}/ohlc/range",  # 💼 Coin OHLC Chart within Time Range by ID
+    "/coins/categories",  # Coins Categories List with Market Data
+
+    # Onchain
+    "/onchain/simple/networks/{network}/token_price/{addresses}",  # Token Price by Token Addresses
+    "/onchain/networks",  # Supported Networks List (ID Map)
+    "/onchain/networks/{network}/trending_pools",  # Trending Pools by Network
+    "/onchain/networks/{network}/pools/multi/{addresses}",  # Multiple Pools Data by Pool Addresses
+    "/onchain/networks/{network}/pools",  # Top Pools by Network
+    "/onchain/pools/megafilter",  # 🔥 Megafilter for Pools
+    "/onchain/networks/{network}/tokens/multi/{addresses}"  # Tokens Data by Token Addresses
 ]
 
 
